1c23d3ad5dedd01c27cf26edd20acff7b36065d3
sensini7 <pelekengaih@gmail.com>
2024-11-17 03:46:39 +0100

    enhanced logging

--- EZD.ECI.PassPolicy/Public/Set-PasswordPolicyToNeverExpire.ps1
+++ EZD.ECI.PassPolicy/Public/Set-PasswordPolicyToNeverExpire.ps1
@@ -27,9 +27,26 @@ function Set-DomainPasswordPolicy {
             $DriftCounter++
             $DriftSummary += "Domain $($DomainId): CURRENT: $($CurrentPasswordValidityPeriodInDays) -> DESIRED: $($DesiredPasswordValidityPeriodInDays)"
             Write-Host "The password policy for domain $DomainId is not configured as desired."
-            Write-Host "The current password validity period is $CurrentPasswordValidityPeriodInDays days. It should be set to $DesiredPasswordValidityPeriodInDays days."
+            
+            # Enhanced logging for current password validity period
+            if ($CurrentPasswordValidityPeriodInDays -eq 0) {
+                Write-Host "The current password validity period is 0 days, meaning it never expires."
+            } else {
+                Write-Host "The current password validity period is $CurrentPasswordValidityPeriodInDays days, meaning it expires in $CurrentPasswordValidityPeriodInDays days."
+            }
+
+            # Enhanced logging for desired password validity period
+            if ($DesiredPasswordValidityPeriodInDays -eq 0) {
+                Write-Host "It should be set to 0 days, meaning it should never expire."
+            } else {
+                Write-Host "It should be set to $DesiredPasswordValidityPeriodInDays days, meaning it should expire in $DesiredPasswordValidityPeriodInDays days."
+            }
         } else {
-            Write-Host "The Current Password policy for domain $DomainId is $DesiredPasswordValidityPeriodInDays which matches the desired configuration. No change is necessary."
+            if ($DesiredPasswordValidityPeriodInDays -eq 0) {
+                Write-Host "The current password policy for domain $DomainId is set to never expire, which matches the desired configuration. No change is necessary."
+            } else {
+                Write-Host "The current password policy for domain $DomainId is set to expire in $DesiredPasswordValidityPeriodInDays days, which matches the desired configuration. No change is necessary."
+            }
         }
     }
 
